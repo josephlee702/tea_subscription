3d93e7c59a90bb63de02b35a46c9e63d270b6681
Joseph Lee <jhjlee702@gmail.com>
2024-01-28 15:29:38 -0700

    add factorybot and create subscription controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :api do
+    namespace :v0 do
+      #An endpoint to subscribe a customer to a tea subscription
+      get "/subscribe", to: "subscription#subscribe"
+      #An endpoint to cancel a customer’s tea subscription
+      #An endpoint to see all of a customer’s subsciptions (active and cancelled)
+    end
+  end
 end

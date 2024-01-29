ee6d4e947c7ec05721c9da9eac9cb1f93126db2f
Joseph Lee <jhjlee702@gmail.com>
2024-01-28 21:39:39 -0700

    finish endpoint for viewing all subs for a customer

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
     namespace :v0 do
       post "/subscriptions", to: "subscriptions#create"
       patch "/subscription_cancel", to: "subscriptions#cancel"
-      #An endpoint to cancel a customer’s tea subscription
-
-      #An endpoint to see all of a customer’s subsciptions (active and cancelled)
     end
   end
 end
